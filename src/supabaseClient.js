4b5fc0a8d52b93830bdfa961a3b15e007a82a87a
JaceLander <jacethelander@gmail.com>
2025-06-03 14:08:52 -0400

    just did local

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = 'https://brebcyfwyqlefbbaussq.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJyZWJjeWZ3eXFsZWZiYmF1c3NxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc1MzMyNDgsImV4cCI6MjA2MzEwOTI0OH0.WEBzldKxCK4jIGauV3YzlP7BI_h-vdlI9stKwd5Q8eE';
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
