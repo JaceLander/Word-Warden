b43976492b9a6a19c14cb9ec291914e006a53ecd
JaceLander <jacethelander@gmail.com>
2025-06-04 12:27:38 -0400

    fixed a couple thangs

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import supabase from './supabaseClient'
 import words from './wordlistscript'
 import insertGuess from './insertScripts'
+import React, { useState } from 'react'
 
 
 
@@ -13,71 +14,60 @@ import insertGuess from './insertScripts'
   return (
     <div className="App">
     <header>Word Warden</header>
-    <input type='text' id='name'></input><br></br>
+    <input type='text' className='box' id='name'></input><br></br>
     <text>Please enter word</text><br></br>
     <input type='text' id='answer'></input><br></br>
-    <input type='button' id='submit' onClick={click()}></input>
+    <input type='button' id='submit' onClick={() => CheckButton()}></input>
     </div>
 
 
   );
 }
 
-  async function CheckButton() {
-    var correct = true;
-    const { data, error } = await supabase
-     .from('SuccessfulGuess')
-     .select('*');
-     const guessList = data.map(row => row.guess);
-     if(error)
-     console.log("error: " + error.message);
+
+
+//const button = document.getElementById("submit");
+
+async function CheckButton() {
+
+  const { data } = await supabase
+   .from('SuccessfulGuess')
+   .select('*');
+
+   const guessList = data.map(row => row.guess);
 
 
 
-  const guessText = document.getElementById("answer").value.toLowerCase();
-  const username = document.getElementById("name").value.toLowerCase();
-  const button = document.getElementById("submit");
-  if (username.length == 0) {
-    alert("Please enter name");
+const guessText = document.getElementById("answer").value.toLowerCase();
+const username = document.getElementById("name").value.toLowerCase();
+if (username.length === 0) {
+  alert("Please enter name");
+  return;
+  }
   if (guessText.length !== 5) {
     alert("You must provide a word with 5 letters");
-    } else {
+    return;
+    } 
+    else {
       //checks if word is valid
-      if(words.includes(guessText)){
+      if(words.includes(guessText.toLowerCase())){
       //checks if word has been already guessed
-      if (guessList.includes(guessText)) {
+      if (guessList.includes(guessText.toLowerCase())) {
         const match = data.find(row => row.guess === guessText);
         const guesserName = match?.username ?? "Unknown";
         alert("This word was already claimed by " + guesserName)
+        return;
 
-
-//if word wasn't already guessed
-  } else {
-      insertGuess(guessText, username)
-  }
-  //if word isnt real
-}else{
-  alert("Your guess does not exist in the current valid wordlist!")
-}
-}
-  }
-return correct;
-
-
-
-}
-
-const button = document.getElementById("submit");
-
-function click() {
- var check = CheckButton();
-  button.disabled = true;
-
-  setTimeout(() => {
-    button.disabled = false;
-  }, 5000);
-};
-
+      //if word wasn't already guessed
+          } else {
+            insertGuess(guessText, username)
+        }
+        //if word isnt real
+        }else{
+        alert("Your guess does not exist in the current valid wordlist!")
+        }
+      }
+    }
 
 
 export default App;
