c5d52652b188173bf96f34caa9c961996ac9e6a9
JaceLander <jacethelander@gmail.com>
2025-06-09 16:14:15 -0400

    adding

--- src/App.js
+++ src/App.js
@@ -2,20 +2,46 @@ import logo from './logo.svg';
 import './App.css';
 import supabase from './supabaseClient'
 import words from './wordlistscript'
-import {insertGuess, alertToUser} from './insertScripts'
 import React, { useState, useRef, useEffect } from 'react'
 import StaticExample from './modalscripts';
+import { time } from 'framer-motion';
  
 
 
 
 export default App;
 
+function timeout(delay) {
+  return new Promise( res => setTimeout(res, delay) );
+}
 
 
 function App() {
   const [message, setMessage] = useState('');
   const [isError, setIsError] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
+
+  function alertingUser(){
+    setIsVisible(true);
+    timeout(100)
+  }
+
+  async function insertGuess(guess, name)
+  {
+  const { error } = await supabase
+  .from('SuccessfulGuess')
+  .insert({guess: guess, created_at: Date.now().valueOf, username: name})
+  setMessage('Congratulations! This is a new word!');
+  setIsError(false);
+  setIsVisible(true);
+  
+  
+  if (error) {
+      console.log('Insert error:', error.message);
+    }
+  }
+
+
 
 async function CheckButton() {
 
@@ -29,11 +55,15 @@ const guessText = document.getElementById("answer").value.toLowerCase();
 const username = document.getElementById("name").value.toLowerCase();
 
 if (username.length === 0) {
-  alertToUser('Please Enter a username', true);
+  setMessage('Please Enter a username');
+  setIsError(true);
+  setIsVisible(true);
   return;
   }
   if (guessText.length !== 5) {
-    alertToUser('You must provide a word with 5 letters', true);
+    setMessage('Please enter a word that is 5 letters long!');
+    setIsError(true);
+    setIsVisible(true);
     return;
     } 
     else {
@@ -44,23 +74,23 @@ if (username.length === 0) {
         //if guess already existed
           const match = data.find(row => row.guess === guessText);
           const guesserName = match?.username ?? "Unknown";
-          alertToUser('This word was already claimed by ' + guesserName, false);
+          setMessage('This word was already guessed by ' + guesserName);
+          setIsError(true);
+          setIsVisible(true);
         return;
 
       //if word wasn't already guessed
           } else {
-            insertGuess(guessText, username)
+            insertGuess(guessText, username);
         }
         //if word isnt real
         }else{
-          alertToUser('This word does not exist!', true);
+          setMessage('This word does not exist in the current wordlist!');
+          setIsError(true);
+          setIsVisible(true);
         }
       }
     }
-
-
-
-
   return (
     <div className="App" id='general'>
 
@@ -74,13 +104,17 @@ if (username.length === 0) {
             id='answer' 
             placeholder='Word'
             className='box element answer general-font'></input><br></br>
-      <input type='text' 
-            id='response' 
-            placeholder='word'
-            className='box response general-font' readOnly></input><br></br>
+            
+          <div className={`general-font 
+            ${isError ? 'error' : ''}
+            ${isVisible ? '' : 'hidden'}`}>
+            {message}</div>
+      <br></br>
       <input type='button' 
             id='submit' 
-            className='button'
+            className={`button
+            ${isVisible ? 'button-waiting' : ''}`}
+
             onClick={() => CheckButton()}
             ></input>
     </div>
